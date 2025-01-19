6f51f75720d8454e60788b24a52dcbb040456fc8
oyatullohm <marziyahoni@gmail.com>
2025-01-19 22:47:17 +0500

    f

--- Admin/settings.py
+++ Admin/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-x=^wkt=hfs8%1qk3x3rg*(bq_k_mhtuq!avgoy!rb8m4%zfkx_'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -116,9 +116,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
-STATICFILES_DIRS = [BASE_DIR/'static/']
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join('static')
+STATICFILES_DIRS = (
+     os.path.join(BASE_DIR / "staticfiles"),
+)
 MEDIA_URL = 'media/'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
